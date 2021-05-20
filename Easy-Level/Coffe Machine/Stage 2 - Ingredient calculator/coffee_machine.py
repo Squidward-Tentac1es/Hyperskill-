c1635a61ab0b3bd029fb4479c64a4e6b6a29166d
Squidward-Tentac1es <deahuseyn@mail.ru>
2021-05-20 21:43:02 +0300

    com

---
+++ Easy-Level/Coffe Machine/Stage 2 - Ingredient calculator/coffee_machine.py
@@ -0,0 +1,6 @@
+# Write your code here
+question = int(input('Write how many cups of coffee you will need:\n'))
+print('For ' + str(question) + ' cups of coffee you will need:')
+print(str(question * 200) + ' ml of water')
+print(str(question * 50) + ' ml of milk')
+print(str(question * 15) + ' g of coffee beans')
