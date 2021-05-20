4d42f779081a055de99facf9ead8f9389663f42f
Squidward-Tentac1es <deahuseyn@mail.ru>
2021-05-20 21:14:52 +0300

    com

---
+++ Easy-Level/Simple Chatty Bot/Stage 2 - Print your name/simple_chatty_bot.py
@@ -0,0 +1,8 @@
+a = input()
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your ' + a + '.')
+
+# reading a name
+
+print('What a great name you have, ' + a + '!')
